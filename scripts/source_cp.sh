f09c772386aeeaaa6fe2cbe662a640071a72a55c
Naotaka Jay HOTTA <fukuzo@cybertron.co.jp>
2017-06-13 18:00:40 +0900

    added copy script

---
+++ scripts/source_cp.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+cp -r $HOME/DD-support/documentation/content $HOME/DD-support/dd-omegat/source/dd-docs/
+
+rm -rf $HOME/SandBoxs/dd-omegat/source/dd-docs/content/ja
+rm -rf $HOME/SandBoxs/dd-omegat/source/dd-docs/content/tipuesarch
+rm -rf $HOME/SandBoxs/dd-omegat/source/dd-docs/content/static
+rm $HOME/SandBoxs/dd-omegat/source/dd-docs/content/sitemap.xml
+
