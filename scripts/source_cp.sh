5ddfff3cc45f42777ccc7a287b196127f2fe27b8
Naotaka Jay HOTTA <fukuzo@cybertron.co.jp>
2017-06-13 18:13:13 +0900

    update copy script

--- scripts/source_cp.sh
+++ scripts/source_cp.sh
@@ -6,4 +6,5 @@ rm -rf $HOME/SandBoxs/dd-omegat/source/dd-docs/content/ja
 rm -rf $HOME/SandBoxs/dd-omegat/source/dd-docs/content/tipuesearch
 rm -rf $HOME/SandBoxs/dd-omegat/source/dd-docs/content/static
 rm $HOME/SandBoxs/dd-omegat/source/dd-docs/content/sitemap.xml
+git status -s
 
